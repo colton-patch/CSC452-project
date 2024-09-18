d266d6a41f0c019d9d52d45e5f05a24289f8c782
Colton Patch <coltondp@gmail.com>
2024-09-17 19:53:21 -0700

    called USLOSS_ContextInit for init

--- phase1.c
+++ phase1.c
@@ -3,6 +3,15 @@
  */
 
 #include <phase1.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// prototypes
+void startFuncWrapper(void);
+int startFuncInit(void);
+void checkForKernelMode(void);
+unsigned int disableInterrupts(void);
 
 // structure for a process control block. Contains PID, name, priority, current context, the process' 
 // start function and argument, and pointers to its parent, youngest child, and next older sibling
@@ -11,7 +20,7 @@ struct pcb {
 	char name[MAXNAME];
 	int priority;
 	USLOSS_Context *context;
-	int (*startFunc)(void *);
+	int (*startFunc)();
 	void *arg;
 	struct pcb *parent;
 	// each process points to its youngest child, which points to its next older sibling and so on
@@ -19,10 +28,12 @@ struct pcb {
 	struct pcb *nextOlderSibling;
 };
 
+
 // global variables
 int curId = 1; // The ID of the next process
 struct pcb *pcbTable; // table of PCBs
 struct pcb *curProc; // currently running process
+
 
 /*
 * void phase1_init(void) - creates the PCB table and the PCB structure for the 
@@ -39,11 +50,16 @@ void phase1_init(void) {
 
 	// make pcb entry for init
 	pcbTable[0].pid = curId;
-	pcbTable[0].name = &"init";
+	strcpy(pcbTable[0].name, "init");
 	pcbTable[0].priority = 6;
 	pcbTable[0].startFunc = &startFuncInit; // init's start function
 	pcbTable[0].arg = NULL;
 	curId++;
+
+	// initialize context for init
+	// --THIS MAY BE IN THE WRONG PLACE-- can we malloc() here?
+	void *newStack = malloc(USLOSS_MIN_STACK);
+	USLOSS_ContextInit(pcbTable[0].context, newStack, USLOSS_MIN_STACK, NULL, &startFuncWrapper);
 
 	// restore interrupts
 	USLOSS_PsrSet(prevPsr);
@@ -69,7 +85,7 @@ int join(int *status) {
 	USLOSS_PsrSet(prevPsr);
 }
 
-void quit_phase1a(int status, int switchToPid) {
+void quit_phase_1a(int status, int switchToPid) {
 	// make sure in kernel mode and disable interrupts
 	checkForKernelMode();
 	unsigned int prevPsr = disableInterrupts();
@@ -123,7 +139,7 @@ void TEMP_switchTo(int pid) {
 * void startFuncWrapper(void) - wrapper for any process' start function. It will call the 
 * 	start function of the current process, then quit() if that function returns.
 */
-static void startFuncWrapper(void) {
+void startFuncWrapper(void) {
 	int (*startFunc)(void *) = curProc->startFunc;
 	void *arg = curProc -> arg;
 	int status = (*startFunc)(arg);
@@ -134,22 +150,23 @@ static void startFuncWrapper(void) {
 * void startFuncInit(void) - The start function for the init process. It calls spork() to
 *	create the testcase_main process, then repeatedly calls join() until it returns -2.
 */
-static void startFuncInit(void) {
-	int retval = spork("testcase_main", &testcase_main, NULL, USLOSS_MIN_STACK, 3);
+int startFuncInit(void) {
+	spork("testcase_main", &testcase_main, NULL, USLOSS_MIN_STACK, 3);
 
 	int joinVal = 0;
-	int *joinStatus;
+	int *joinStatus = 0;
 	while (joinVal != -2) {
 		joinVal = join(joinStatus);
 	}
 
-	USLOSS_Trace("ERROR: init process has no children"); 
+	USLOSS_Trace("ERROR: init process has no children");
+	return 1;
 }
 
 /*
 * void checkForKernelMode(void) - Halts if not currently in kernel mode 
 */
-static void checkForKernelMode(void) {
+void checkForKernelMode(void) {
 	// check if not in kernel mode
 	if ( (USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0 ) {
 		USLOSS_Trace("ERROR: kernel function called from user mode");
@@ -161,7 +178,7 @@ static void checkForKernelMode(void) {
 * unsigned int disableInterrupts(void) - sets interrupts to disables and returns the previous state
 *	of the PSR.
 */
-static unsigned int disableInterrupts(void) {
+unsigned int disableInterrupts(void) {
 	unsigned int prevPsr = USLOSS_PsrGet();
 	USLOSS_PsrSet(prevPsr & ~USLOSS_PSR_CURRENT_INT);
 	return prevPsr;
